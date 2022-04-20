af42980bd085276cd398de803b329334f4d5d420
Stefan Bloemheuvel <40235998+StefanBloemheuvel@users.noreply.github.com>
2022-04-20 11:51:54 +0200

    Update main_gcn.py

--- main_gcn.py
+++ main_gcn.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import random
+
 import tensorflow as tf
 from tensorflow.keras.callbacks import *
 from tensorflow.keras import models
@@ -12,36 +13,125 @@ from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
 from tensorflow.keras import initializers
+
 from spektral.layers import GCNConv
 from tensorflow.keras.layers import *
 from spektral.utils import gcn_filter
 from sklearn.model_selection import train_test_split
 import datetime
-import sys
-from helper_functions import *
+from spektral.layers import GlobalAvgPool, GlobalMaxPool
+
+import sys 
 
 network_choice = sys.argv[1]
 model_chosen = sys.argv[2]
 random_state_here = int(sys.argv[3])
 
-def build_model(input_shape):
+print('model = ',model_chosen)
+print('network = ',network_choice)
+print('random state = ',random_state_here)
+
+seed = 1
+def print_time():
+    parser = datetime.datetime.now() 
+    return parser.strftime("%d-%m-%Y %H:%M:%S")  
+
+def normalize(inputs): # Houden
+    normalized = []
+    for eq in inputs:
+        maks = np.max(np.abs(eq))
+        if maks != 0:
+            normalized.append(eq/maks)
+        else:
+            normalized.append(eq)
+    return np.array(normalized)      
+
+def targets_to_list(targets): # Houden
+    targets = targets.transpose(2,0,1)
+
+    targetList = []
+    for i in range(0, len(targets)):
+        targetList.append(targets[i,:,:])
+        
+    return targetList
+
+
+seed = 1
+import tensorflow
+def k_fold_split(inputs, targets): # houden
+
+    # make sure everything is seeded
+    import os
+    os.environ['PYTHONHASHSEED']=str(seed)
+    import random
+    random.seed(seed)
+    np.random.seed(seed)
+    np.random.permutation(seed)
+    tensorflow.random.set_seed(seed)
+    
+    p = np.random.permutation(len(targets))
+    
+    print('min of p = ',np.array(p)[50:100].min())
+    print('max of p = ',np.array(p)[50:100].max())
+    print('mean of p = ',np.array(p)[50:100].mean())
+    inputs = inputs[p]
+    targets = targets[p]
+
+    
+    ind = int(len(inputs)/5)
+    inputsK = []
+    targetsK = []
+
+    for i in range(0,5-1):
+        inputsK.append(inputs[i*ind:(i+1)*ind])
+        targetsK.append(targets[i*ind:(i+1)*ind])
+
+    
+    inputsK.append(inputs[(i+1)*ind:])
+    targetsK.append(targets[(i+1)*ind:])
+  
+    
+    return inputsK, targetsK
+        
+def merge_splits(inputs, targets, k): # houden
+    if k != 0:
+        z=0
+        inputsTrain = inputs[z]
+        targetsTrain = targets[z]
+    else:
+        z=1
+        inputsTrain = inputs[z]
+        targetsTrain = targets[z]
+
+    for i in range(z+1, 5):
+        if i != k:
+            inputsTrain = np.concatenate((inputsTrain, inputs[i]))
+            targetsTrain = np.concatenate((targetsTrain, targets[i]))
+    
+    return inputsTrain, targetsTrain, inputs[k], targets[k]
+
+def build_model(input_shape): # houden
+
+    reg_const = 0.0001
+    activation_func = 'relu'
 
     wav_input = layers.Input(shape=input_shape, name='wav_input')
     graph_input = layers.Input(shape=(39,39), name='graph_input')
     graph_features = layers.Input(shape=(39,2), name='graph_features')
 
-    conv1 = layers.Conv1D(filters=32, kernel_size=125, strides=2,  activation='relu', kernel_regularizer=regularizers.l2(0.0001), name='conv1')(wav_input)
-    conv1 = layers.Conv1D(filters=64, kernel_size=125, strides=2,  activation='relu', kernel_regularizer=regularizers.l2(0.0001), name='conv2')(conv1)
-    conv1_new = tf.keras.layers.Reshape((39,conv1.shape[2] * conv1.shape[3]))(conv1)    
+    conv1 = layers.Conv1D(filters=32, kernel_size=125, strides=2,  activation=activation_func, kernel_regularizer=regularizers.l2(reg_const), name='conv1')(wav_input)
+    conv1 = layers.Conv1D(filters=64, kernel_size=125, strides=2,  activation=activation_func, kernel_regularizer=regularizers.l2(reg_const), name='conv2')(conv1)
 
+    conv1_new = tf.keras.layers.Reshape((39,conv1.shape[2] * conv1.shape[3]))(conv1)    
     if model_chosen == 'nofeatures':
         print('went for no features version')
     if model_chosen == 'main':
+
         print('went for features version')
         conv1_new = layers.concatenate(inputs=[conv1_new, graph_features], axis=2)
 
-    conv1_new = GCNConv(64, activation='relu', use_bias=False, kernel_regularizer=regularizers.l2(0.0001))([conv1_new, graph_input])
-    conv1_new = GCNConv(64, activation='tanh', use_bias=False, kernel_regularizer=regularizers.l2(0.0001))([conv1_new, graph_input])
+    conv1_new = GCNConv(64, activation='relu', use_bias=False, kernel_regularizer=regularizers.l2(reg_const))([conv1_new, graph_input])
+    conv1_new = GCNConv(64, activation='tanh', use_bias=False, kernel_regularizer=regularizers.l2(reg_const))([conv1_new, graph_input])
 
     conv1_new = layers.Flatten()(conv1_new)
     conv1_new = layers.Dropout(0.4, seed=seed)(conv1_new)
@@ -57,18 +147,22 @@ def build_model(input_shape):
     final_model = models.Model(inputs=[wav_input, graph_input, graph_features], outputs=[pga, pgv, sa03, sa10, sa30]) #, pgv, sa03, sa10, sa30
     rmsprop = optimizers.RMSprop(learning_rate=0.0001, rho=0.9, epsilon=None, decay=0.)
     final_model.compile(optimizer=rmsprop, loss='mse')
+    
     return final_model
 
 from tensorflow import keras
+
 es = keras.callbacks.EarlyStopping(patience=10, verbose=0, min_delta=0.001, monitor='val_loss', mode='min',baseline=None, restore_best_weights=True)
+
 import sys
+
 #%%
-test_set_size = 0.2
 
 if network_choice == 'network1':
+    test_set_size = 0.2
     inputs = np.load('data/inputs_ci.npy', allow_pickle = True)
     targets = np.load('data/targets.npy', allow_pickle = True)
-  
+    
     graph_input = np.load('data/minmax_normalized_laplacian.npy', allow_pickle=True)
     graph_input = np.array([graph_input] * inputs.shape[0])
 
@@ -76,20 +170,27 @@ if network_choice == 'network1':
     graph_features = np.array([graph_features] * inputs.shape[0])
 
 if network_choice == 'network2':
+    test_set_size = 0.2
     inputs = np.load('data/othernetwork/inputs_cw.npy', allow_pickle = True)
     targets = np.load('data/othernetwork/targets.npy', allow_pickle = True)
     
     graph_input = np.load('data/othernetwork/minmax_normalized_laplacian.npy', allow_pickle=True)
+    
     graph_input = np.array([graph_input] * inputs.shape[0])
 
     graph_features = np.load('data/othernetwork/station_coords.npy', allow_pickle=True)
     graph_features = np.array([graph_features] * inputs.shape[0])
 
 train_inputs, test_inputs, traingraphinput , testgraphinput, train_graphfeature, test_graphfeature, train_targets, testTargets = train_test_split(inputs,graph_input, graph_features, targets, test_size=test_set_size, random_state=random_state_here)
-testInputs, testMaxes = normalize(test_inputs[:, :, :1000, :])        
+testInputs = normalize(test_inputs[:, :, :1000, :])        
 
 import math
+
+
 inputsK, targetsK = k_fold_split(train_inputs, train_targets)
+
+
+
 
 mse_list = []
 rmse_list = []
@@ -108,8 +209,13 @@ for k in range(0,5):
     val_graphinput = traingraphinput[0:valInputsAll.shape[0],:,:]
     val_graphfeatureinput = train_graphfeature[0:valInputsAll.shape[0],:,:]
 
-    trainInputs, trainMaxes = normalize(trainInputsAll[:, :, :1000, :])
-    valInputs, valMaxes = normalize(valInputsAll[:, :, :1000, :])
+    if network_choice == 'network1':
+        trainInputs = normalize(trainInputsAll[:, :, :1000, :])
+        valInputs = normalize(valInputsAll[:, :, :1000, :])
+
+    if network_choice == 'network2':
+        trainInputs = normalize(trainInputsAll[:, :, :, :])
+        valInputs = normalize(valInputsAll[:, :, :, :])
 
     model = build_model(valInputs[0].shape)
 
@@ -120,14 +226,13 @@ for k in range(0,5):
         save_best_only=True
     )
 
-    # print(model.summary())
+    print(model.summary())
     
     history = model.fit(x=[trainInputs,train_graphinput,train_graphfeatureinput], 
-                        y=targets_to_list(trainTargets),#trainTargets[:,:,0],
+                        y=targets_to_list(trainTargets),
             epochs=100, batch_size=20,
             validation_data=([valInputs,val_graphinput,val_graphfeatureinput], targets_to_list(valTargets)),verbose=0,callbacks=[es,iteration_checkpoint])#
     
-
     print()
     print('total number of epochs ran = ',len(history.history['loss']))
     print('Fold number:' + str(k))
@@ -137,33 +242,30 @@ for k in range(0,5):
     new_predictions = np.swapaxes(new_predictions,0,2)
     new_predictions = np.swapaxes(new_predictions,0,1)
     
-    np.save(f'saved_results/gcn_{network_choice}_iteration_{k}_targets.npy',testTargets)
-    np.save(f'saved_results/gcn_{network_choice}_iteration_{k}_predictions.npy',new_predictions)
-
     from sklearn.metrics import mean_squared_error, mean_absolute_error
-    def calculate_results(ground_truth,predictions):
-        MSE = []
-        for i in range(0,5):
-            MSE.append(mean_squared_error(ground_truth[:,:,i], predictions[:,:,i]))
-        MSE = np.array(MSE).mean()
-        
-        RMSE = []
-        for i in range(0,5):
-            RMSE.append(mean_squared_error(ground_truth[:,:,i], predictions[:,:,i], squared=False))
-        RMSE = np.array(RMSE).mean()
-        
-        MAE = []
-        for i in range(0,5):
-            MAE.append(mean_absolute_error(ground_truth[:,:,i], predictions[:,:,i]))
-        MAE = np.array(MAE).mean()
-        
-        mse_list.append(MSE)
-        rmse_list.append(RMSE)
-        mae_list.append(MAE)
-        
-    calculate_results(testTargets,new_predictions)
+    MSE = []
+    for i in range(0,5):
+        MSE.append(mean_squared_error(testTargets[:,:,i], new_predictions[:,:,i]))
+    print('mse = ',np.array(MSE).mean())
+    MSE = np.array(MSE).mean()
+    
+    RMSE = []
+    for i in range(0,5):
+        RMSE.append(mean_squared_error(testTargets[:,:,i], new_predictions[:,:,i], squared=False))
+    print('rmse = ',np.array(RMSE).mean())
+    RMSE = np.array(RMSE).mean()
+    
+    MAE = []
+    for i in range(0,5):
+        MAE.append(mean_absolute_error(testTargets[:,:,i], new_predictions[:,:,i]))
+    print('MAE = ',np.array(MAE).mean())
+    MAE = np.array(MAE).mean()
+    
 
-    # reset keras and tensorflow
+    mse_list.append(MSE)
+    rmse_list.append(RMSE)
+    mae_list.append(MAE)
+
     keras.backend.clear_session()
     tf.keras.backend.clear_session()
 
@@ -183,6 +285,4 @@ with open("githubresults.csv", "a") as text_file:
         print(f'{print_time()},{sys.argv[0]},PSA1,{network_choice},{mse_list[3]},{rmse_list[3]},{mae_list[3]},{random_state_here}', file=text_file)
         print(f'{print_time()},{sys.argv[0]},PSA3,{network_choice},{mse_list[4]},{rmse_list[4]},{mae_list[4]},{random_state_here}', file=text_file)
 
-
-
-
+#%%
